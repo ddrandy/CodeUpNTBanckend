e2c22de772816302d30b27f507499038d50f42f4
Randy <dddrandy@gmail.com>
2025-04-08 15:31:44 +0930

    Git commit in VSCode

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  if (req.url === '/') {
+  if (req.url === '/') { 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World!\n');
   } else if (req.url === '/about') {
