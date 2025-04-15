f97866566fb7784b23afd1e8c62f5f13e7fadfe9
Randy <dddrandy@gmail.com>
2025-04-16 08:01:04 +0930

    Add get route /products.

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ app.get('/', (req, res) => { res.send('Hello world!') });
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/about.html')
 });
+
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found.' });
+});
+
+let products = [
+  { 'product_id': 1, 'name': 'Hat', 'price': 12 },
+  { 'product_id': 2, 'name': 'Gloves', 'price': 18 },
+  { 'product_id': 3, 'name': 'Glasses', 'price': 22 },
+];
+app.get('/products', (req, res) => {
+  res.json(products)
 });
 app.listen(3000, () => { console.log("Server running on http://localhost:3000") });
