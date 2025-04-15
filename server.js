59437ae51df9f8edbef6d7fbc6aa217ee6f6da15
Randy <dddrandy@gmail.com>
2025-04-15 12:17:22 +0930

    Add route /products Changed to ES mode

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  if (req.url === '/') { 
+import { createServer } from 'http';
+let products = [
+  { 'product_id': 1, 'name': 'Hat', 'price': 12 },
+  { 'product_id': 2, 'name': 'Gloves', 'price': 18 },
+  { 'product_id': 3, 'name': 'Glasses', 'price': 22 },
+];
+createServer((req, res) => {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World!\n');
   } else if (req.url === '/about') {
@@ -17,6 +21,19 @@ http.createServer((req, res) => {
         </body>
       </html>
       `);
+  } else if (req.url.startsWith('/products')) {
+    if (req.method === 'GET') {
+      res.writeHead(200, { "Content-Type": 'application/json' })
+      res.end(JSON.stringify(products))
+    } else if (req.method === 'POST') {
+      res.writeHead(200, { "Content-Type": 'application/json' })
+      let body = '';
+      req.on('data', chunk => body += chunk);
+      req.on('end', ()=>{
+        products.push(JSON.parse(body));
+        res.end(JSON.stringify(products));
+      })
+    }
   }
 }).listen(3001);
 
