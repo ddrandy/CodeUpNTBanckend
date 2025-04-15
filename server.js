2f218dc9da34639e15dd8a80e9115afd39cf7634
Randy <dddrandy@gmail.com>
2025-04-16 07:58:36 +0930

    Add nodemon dependency.

--- server.js
+++ server.js
@@ -1,40 +1,11 @@
-import { createServer } from 'http';
-let products = [
-  { 'product_id': 1, 'name': 'Hat', 'price': 12 },
-  { 'product_id': 2, 'name': 'Gloves', 'price': 18 },
-  { 'product_id': 3, 'name': 'Glasses', 'price': 22 },
-];
-createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!\n');
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(`
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>About</title>
-        </head>
-        <body>
-          <p>This project is a backend for an online shopping mall ... , built with Node.js.</p>
-        </body>
-      </html>
-      `);
-  } else if (req.url.startsWith('/products')) {
-    if (req.method === 'GET') {
-      res.writeHead(200, { "Content-Type": 'application/json' })
-      res.end(JSON.stringify(products))
-    } else if (req.method === 'POST') {
-      res.writeHead(200, { "Content-Type": 'application/json' })
-      let body = '';
-      req.on('data', chunk => body += chunk);
-      req.on('end', ()=>{
-        products.push(JSON.parse(body));
-        res.end(JSON.stringify(products));
-      })
-    }
-  }
-}).listen(3001);
+const express = require('express');
 
-console.log('Server running on port 3001');
+const app = express();
+app.get('/', (req, res) => { res.send('Hello world!') });
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html')
+});
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Route not found.' });
+});
+app.listen(3000, () => { console.log("Server running on http://localhost:3000") });
